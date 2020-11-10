cb193baf6934dd4a1becc8a01cc4b72e1c218eed
SpyrosGeo <xarodasimba@gmail.com>
2020-11-10 19:45:26 +0200

    finished authentication for register and login//Next: move authentication logit to middleware and create/delete posts maybe

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,14 +2,46 @@ const bcrypt = require('bcryptjs')
 const jtw = require('jsonwebtoken')
 const {UserInputError} = require('apollo-server')
 
-const {validateRegisterInput} =require('../../utils/validators')
+const {validateRegisterInput,validateLoginInput} =require('../../utils/validators')
 const User = require('../../models/User')
 const {SECRET_KEY} = require('../../config')
 
 
+function generateToken(user){
+    return jtw.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY, { expiresIn: '1h' })
+}
 
 module.exports = {
     Mutation:{
+        async login(_,{username,password}){
+          const {errors,valid} = validateLoginInput(username,password)
+          if(!valid){
+              throw new UserInputError('Wrong Credentials', { errors })
+          }
+          const user = await User.findOne({username})
+          if(!user) {
+              errors.general = 'User not found'
+                throw new UserInputError('User not found',{errors})
+            }  
+            const match = await bcrypt.compare(password,user.password)
+            if(!match){
+                errors.general = 'Wrong Credentials'
+                throw new UserInputError('Wrong Credentials', { errors })
+            }
+            //login is correct at this point
+            //issue a token
+            const token = generateToken(user)
+            return {
+                ...user._doc,
+                id: user.id,
+                token
+            }
+
+        },
         async register(_,{registerInput:{username,password,email,confirmPassword}},context,info){
             //validate user data
             const {valid,errors} = validateRegisterInput(username,email,password,confirmPassword)
@@ -37,11 +69,7 @@ module.exports = {
 
             })
             const res = await newUser.save()
-            const token = jtw.sign({
-                id:res.id,
-                email:res.email,
-                username:res.username
-            },SECRET_KEY,{expiresIn:'1h'})
+            const token = generateToken(res)
             return {
                 ...res._doc,
                 id:res.id,
