b93f1575c2cd2a79d5b52db00508c39359dde3f2
SpyrosGeo <xarodasimba@gmail.com>
2020-11-11 21:25:48 +0200

    added like/unlike functionality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,22 +1,22 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post')
 const checkAuth = require('../../utils/check-auth')
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find().sort({createdAt:-1}); //sort is to show posts from latest to oldest
+                const posts = await Post.find().sort({ createdAt: -1 }); //sort is to show posts from latest to oldest
                 return posts;
             } catch (err) {
                 throw new Error(err);
             }
         },
-        async getPost(_,{postId}){
+        async getPost(_, { postId }) {
             try {
                 const post = await Post.findById(postId);
-                if(post){
+                if (post) {
                     return post
-                }else {
+                } else {
                     throw new Error('Post Not Found')
                 }
             } catch (err) {
@@ -24,33 +24,57 @@ module.exports = {
             }
         }
     },
-    Mutation:{
-        async createPost(_,{body},context){
+    Mutation: {
+        async createPost(_, { body }, context) {
             //context is req forwarded from the index.js 
             const user = checkAuth(context)
             // console.log(user)
             const newPost = new Post({
                 body,
-                user:user.id,
-                username:user.username,
+                user: user.id,
+                username: user.username,
                 createdAt: new Date().toISOString()
             })
-            const post =await newPost.save()
+            const post = await newPost.save()
             return post
         },
-        async deletePost(_,{postId},context){
+        async deletePost(_, { postId }, context) {
             const user = checkAuth(context)
             try {
                 const post = await Post.findById(postId)
-                if(user.username === post.username){
+                if (user.username === post.username) {
                     await post.delete()
                     return 'Post deleted successfully'
-                }else {
+                } else {
                     throw new AuthenticationError('Action not allowed')
                 }
             } catch (error) {
                 throw new Error(error)
             }
+        },
+        likePost: async (_, { postId }, context) => {
+            const { username } = checkAuth(context)
+            const post = await Post.findById(postId)
+
+            if (post) {
+                if (post.likes.find(like => like.username === username)) {
+                    //post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username)
+
+                } else {
+                    //Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save()
+                return post
+            } else throw new UserInputError('Post not found')
+
         }
-    }
+
+
+    },
+
 }
